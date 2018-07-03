ef187732e96f2652e09645f245c874a4cdbef8d5
Sonali Bhavsar <sonalibhavsar977@gmail.com>
2018-07-03 15:03:09 +0530

    Check

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  def index
+    @content_first = 'first blog'
+    @content_second = 'second blog'
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+
+
+
+end
