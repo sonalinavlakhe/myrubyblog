5362100a9043a379cf92d69a89ca87936497028e
Sonali Bhavsar <sonalibhavsar977@gmail.com>
2018-07-05 17:48:43 +0530

    Updated app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_03_104103) do
+ActiveRecord::Schema.define(version: 2018_07_04_120918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,24 +1,7 @@
 require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get categories_index_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get categories_new_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get categories_edit_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get categories_show_url
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
